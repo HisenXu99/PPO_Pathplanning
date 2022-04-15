63c9889bc67853de47991b58519ac2d35dcbb470
HisenXu99 <1610598376@qq.com>
2022-04-15 11:05:00 +0800

    first commit

---
+++ Empty/PPO_A2C.py
@@ -0,0 +1,260 @@
+# -*- coding: UTF-8 -*-
+#没写oldpi网络的情况
+from cv2 import merge
+import tensorflow as tf
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+from tensorflow import keras
+import time
+import cv2
+import os
+import math
+import sys
+
+
+# A_LR = 0.001
+# A_DIM =11
+# UPDATE_STEPS = 10
+
+class PPO:
+    def __init__(self,loadpath,LR):
+        self.GAMMA=0.9
+
+#########################################Network#################################
+        # Parameter for LSTM
+        self.Num_dataSize = 364  # 360 sensor size + 4 self state size
+        self.Num_cellState = 512
+        # parameters for skipping and stacking
+        self.Num_skipFrame = 1
+        self.Num_stackFrame = 4
+        # Parameters for CNN
+        self.img_size = 80  # input image size
+        self.first_conv = [8, 8, self.Num_stackFrame, 32]
+        self.second_conv = [4, 4, 32, 64]
+        self.third_conv = [3, 3, 64, 64]
+        self.first_dense = [10*10*64+self.Num_cellState, 512]
+        self.second_dense_state = [self.first_dense[1], 1]
+        self.second_dense_action = [self.first_dense[1], 11]
+
+        self.n_actions=11
+        self.n_features=4
+
+#########################################Train#################################
+        self.ent_coef=0.01
+        self.vf_coef=0.5
+
+        self.lr=LR
+
+
+        # date - hour - minute - second of training time
+        # self.date_time = str(datetime.date.today())
+        self.date_time = datetime.datetime.now().strftime("%Y-%m-%d-%H_%M_%S")
+        os.makedirs( 'Empty/saved_networks/'+self.date_time)
+
+        self.load_path = loadpath
+
+        self.epsilon=0.2
+
+        self.Num_action = 11
+
+
+#######################################Initialize Network###############################
+        self.network()
+        self.sess,self.saver,self.writer=self.init_sess()
+        pass
+
+    def weight_variable(self, shape):
+        return tf.Variable(self.xavier_initializer(shape))
+
+    def bias_variable(self, shape):  # 初始化偏置项
+        return tf.Variable(self.xavier_initializer(shape))
+
+    # Xavier Weights initializer
+    def xavier_initializer(self, shape):
+        dim_sum = np.sum(shape)
+        if len(shape) == 1:
+            dim_sum += 1
+        bound = np.sqrt(2.0 / dim_sum)
+        return tf.random_uniform(shape, minval=-bound, maxval=bound)
+
+    def conv2d(self, x, w, stride):  # 定义一个函数，用于构建卷积层
+        return tf.nn.conv2d(x, w, strides=[1, stride, stride, 1], padding='SAME')
+
+    # Xavier Weights initializer
+    def normc_initializer(self,std=1.0, axis=0):
+        def _initializer(shape, dtype=None, partition_info=None):  # pylint: disable=W0613
+            out = np.random.randn(*shape).astype(dtype.as_numpy_dtype)
+            out *= std / np.sqrt(np.square(out).sum(axis=axis, keepdims=True))
+            return tf.constant(out)
+        return _initializer
+
+
+
+    def network(self):
+        tf.reset_default_graph()
+
+        # Input------image
+        self.x_image = tf.placeholder(tf.float32, shape=[None, self.img_size, self.img_size, self.Num_stackFrame],name="image")
+        self.x_normalize = (self.x_image - (255.0/2)) / (255.0/2)  # 归一化处理
+
+        self.s = tf.placeholder(tf.float32, [None, 4], "state")
+        self.a = tf.placeholder(tf.float32, None, "action")
+        self.td = tf.placeholder(tf.float32, None, "td_error")  # TD_error
+
+        with tf.variable_scope('Network'):
+            with tf.variable_scope('CNN'):
+                w_conv1 = self.weight_variable(self.first_conv)  # w_conv1 = ([8,8,4,32])
+                b_conv1 = self.bias_variable([self.first_conv[3]])  # b_conv1 = ([32])
+
+                # second_conv=[4,4,32,64]
+                w_conv2 = self.weight_variable(self.second_conv)  # w_conv2 = ([4,4,32,64])
+                b_conv2 = self.bias_variable([self.second_conv[3]])  # b_conv2 = ([64])
+
+                # third_conv=[3,3,64,64]
+                w_conv3 = self.weight_variable(self.third_conv)  # w_conv3 = ([3,3,64,64])
+                b_conv3 = self.bias_variable([self.third_conv[3]])  # b_conv3 = ([64])
+
+                h_conv1 = tf.nn.relu(self.conv2d(self.x_normalize, w_conv1, 4) + b_conv1)
+                h_conv2 = tf.nn.relu(self.conv2d(h_conv1, w_conv2, 2) + b_conv2)
+                h_conv3 = tf.nn.relu(self.conv2d(h_conv2, w_conv3, 1) + b_conv3)
+                h_pool3_flat = tf.reshape(h_conv3, [-1, 10 * 10 * 64])  # 将tensor打平到vector中
+
+                cnn_out1 = tf.layers.dense(
+                    inputs=h_pool3_flat,
+                    units=512,    # number of hidden units
+                    activation=tf.nn.relu,
+                    kernel_initializer=tf.random_normal_initializer(0., .1),    # weights
+                    bias_initializer=tf.constant_initializer(0.1),  # biases
+                    name='cnn_out1'
+                )
+                cnn_out2 = tf.layers.dense(
+                    inputs=cnn_out1,
+                    units=self.n_features,    # output units
+                    activation=tf.nn.softmax,   # get action probabilities
+                    kernel_initializer=tf.random_normal_initializer(0., .1),  # weights
+                    bias_initializer=tf.constant_initializer(0.1),  # biases
+                    name='cnn_out2'
+                )
+
+            h_concat = tf.concat([cnn_out2, self.s], axis=1)
+
+
+
+            with tf.variable_scope('AC'):
+                # h_concat = tf.concat([h_pool3_flat, rnn_out], axis=1)
+                with tf.variable_scope('Actor'):
+                    l1 = tf.layers.dense(
+                        inputs=h_concat,
+                        units=512,    # number of hidden units
+                        activation=tf.nn.relu,
+                        kernel_initializer=tf.random_normal_initializer(0., .1),    # weights
+                        bias_initializer=tf.constant_initializer(0.1),  # biases
+                        name='l1'
+                    )
+                    self.acts_prob = tf.layers.dense(
+                        inputs=l1,
+                        units=self.n_actions,    # output units
+                        activation=tf.nn.softmax,   # get action probabilities
+                        kernel_initializer=tf.random_normal_initializer(0., .1),  # weights
+                        bias_initializer=tf.constant_initializer(0.1),  # biases
+                        name='acts_prob'
+                    )
+
+                with tf.variable_scope('Critic'):
+                    l1 = tf.layers.dense(
+                        inputs=h_concat,
+                        units=512,  # number of hidden units
+                        activation=tf.nn.relu,  # None
+                        kernel_initializer=tf.random_normal_initializer(0., .1),  # weights
+                        bias_initializer=tf.constant_initializer(0.1),  # biases
+                        name='l1'
+                    )
+
+                    self.v = tf.layers.dense(
+                        inputs=l1,
+                        units=1,  # output units
+                        activation=None,
+                        kernel_initializer=tf.random_normal_initializer(0., .1),  # weights
+                        bias_initializer=tf.constant_initializer(0.1),  # biases
+                        name='V'
+                    )
+
+        with tf.variable_scope('Train'):
+            self.a = tf.placeholder(tf.float32, [None,self.n_actions], "action")
+            self.td = tf.placeholder(tf.float32, [None,1], "td_error")  # TD_error
+            self.discount_r = tf.placeholder(tf.float32, [None, 1], "v_next")
+            with tf.variable_scope('squared_TD_error'):
+                self.td_error = self.discount_r - self.v
+                self.closs = tf.reduce_mean(tf.square(self.td_error))   # TD_error = (r+gamma*V_next) - V_eval
+            with tf.variable_scope('exp_v'):
+                log_prob = tf.log(tf.reduce_sum(self.acts_prob*self.a,axis = 1 ))
+                self.exp_v = tf.reduce_mean(log_prob * self.td)  # advantage (TD_error) guided loss
+            loss=-self.exp_v+0.5*self.closs
+            self.train_op = tf.train.AdamOptimizer(self.lr).minimize(loss)
+
+        with tf.variable_scope('Record'):
+            aloss=tf.summary.scalar("Actor_loss",self.exp_v )
+            closs=tf.summary.scalar("Critic_loss", self.closs)
+            total_loss=tf.summary.scalar("Total_loss",loss )
+            self.merged=tf.summary.merge_all()  
+        pass
+
+    
+    def init_sess(self):
+        # Initialize variables
+        config = tf.ConfigProto()
+        config.gpu_options.per_process_gpu_memory_fraction = 0.6  # 程序最多只能占用指定gpu50%的显存  
+        sess = tf.InteractiveSession(config=config)
+        init = tf.global_variables_initializer()
+        sess.run(init)
+        saver = tf.train.Saver()
+        # Load the file if the saved file exists
+        check_save = input('Load Model? (1=yes/2=no): ')
+        if check_save == 1:
+            # Restore variables from disk.
+            saver.restore(sess, self.load_path + "/model.ckpt")
+            print("Model restored.")
+            #TODO:这里只是PPO.Num_training
+            check_train = input('Inference or Training? (1=Inference / 2=Training): ')
+            if check_train == 1:
+                self.Num_start_training = 0
+                self.Num_training = 0
+        log_dir = 'Empty/logs/' +self.date_time 
+        writer=tf.summary.FileWriter(log_dir, tf.get_default_graph())
+        return sess,saver,writer
+
+    def choose_action(self,s,image):
+        probs = self.sess.run(self.acts_prob, {self.s:s,self.x_image:image})   # get probabilities for all actions
+        # with open('Empty/saved_networks/'+self.date_time+'/value.txt','a') as f:             
+        #     # f.write('prob')
+        #     f.write('\r')
+        #     np.savetxt(f, probs, delimiter=',', fmt = '%s')
+        return np.random.choice(np.arange(probs.shape[1]), p=probs.ravel())   # return a int
+        pass
+
+
+    def train(self,state_stack,image_stack,rewards_stack,actions_stack,train_step,next_state,next_image):
+        v_s_ = self.sess.run(self.v, {self.s:next_state,self.x_image:next_image})[0,0]
+        #用来看看prob的变化
+        probs = self.sess.run(self.acts_prob, {self.s:next_state,self.x_image:next_image})   # get probabilities for all actions
+        discounted_r = []
+        for r in rewards_stack[::-1]:   #buffer_r[::-1]返回倒序的原list
+            v_s_ = r + self.GAMMA * v_s_
+            discounted_r.append(v_s_)
+        discounted_r.reverse()
+        discounted_r=np.array(discounted_r)[:, np.newaxis]
+        td_error,v= self.sess.run([self.td_error,self.v],
+                                {self.s:state_stack, self.x_image:image_stack,self.discount_r: discounted_r})
+
+        feed_dict = {self.s:state_stack, self.x_image:image_stack, self.a: actions_stack, self.td: td_error, self.discount_r: discounted_r}
+        _, _ = self.sess.run([self.train_op, self.merged], feed_dict)
+        return v_s_,probs
+
+    def save_model(self):
+        # ------------------------------
+        save_path = self.saver.save(
+            self.sess, 'Empty/saved_networks/'+self.date_time + "/model.ckpt")
+        # ------------------------------
+        pass
---
+++ Empty/play_PPO.py
@@ -0,0 +1,338 @@
+# -*- coding: UTF-8 -*-
+import tensorflow as tf
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
+import time
+import cv2
+import os
+import sys
+import math
+from PPO_A2C import PPO
+from Environment_Empty import envmodel
+import json
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'  # 默认显卡0
+
+env = envmodel()
+
+# 动作指令集---> v,w
+action_dict = {0: [1.0, -1.0], 1: [1.0, -0.5], 2: [1.0, 0.0],
+               3: [1.0, 0.5], 4: [1.0, 1.0], 5: [0.5, -1.0],
+               6: [0.5, 0.0], 7: [0.5, 1.0], 8: [0.0, -1.0],
+               9: [0.0, 0.0], 10: [0.0, 1.0]}
+
+
+#TODO：改变学习率，从batch中抽几帧，改变sleep时间，改连续变为离散
+
+class play:
+    def __init__(self):
+        # Get parameters
+        self.progress = ''
+        self.Num_action = len(action_dict)
+        
+        self.date_time = str(datetime.date.today())
+
+        self.loadpath= 'Empty/saved_networks/practice/2022-04-14-03_22_05'
+
+        # Initial parameters
+        # ------------------------------
+        self.Num_start_training = 0
+        self.Num_training = 600000
+        # ------------------------------
+        self.Num_test = 0
+        self.GAMMA = 0.9
+        # self.Final_epsilon = 0.1
+        # ------------------------------
+        # self.Epsilon = 0.5
+        # ------------------------------
+       
+        self.step = 1
+        self.score = 0
+        self.episode = 0
+        # ------------------------------
+
+        # date - hour - minute - second of training time
+        self.date_time = str(datetime.date.today())
+
+        # parameters for skipping and stacking
+        self.Num_skipFrame = 1
+        self.Num_stackFrame = 4
+
+        # Parameters for network
+        self.img_size = 80  # input image size
+
+        # Initialize agent robot
+        self.agentrobot = 'jackal0'
+
+        # Define the distance from start point to goal point
+        self.d = 15.0
+
+        # Define the step for updating the environment
+        self.MAXSTEPS =600
+        # ------------------------------
+        self.MAXEPISODES = 5000
+        # ------------------------------
+
+        # Parameter for LSTM
+        self.Num_dataSize = 364  # 360 sensor size + 4 self state size
+
+        self.Num_batch = 16
+
+        self.LR=0.0001
+
+        # sess, self.saver = self.init_sess()
+        self.PPO=PPO(self.loadpath,self.LR)
+        self.parameter()
+        pass
+
+    def parameter(self):
+        msg={  
+            'Network_parameter':{  
+                'LR':self.LR,  
+                'Num_Node_A':512,
+                'Num_Node_C':512 
+            },  
+            'Train_parameter': {  
+                'Num_batch':self.Num_batch,
+                'Num_training':self.Num_training 
+            }  
+        }
+        js = json.dumps(msg, indent=2) 
+        desktop_path =  'Empty/saved_networks/'+self.PPO.date_time+'/'  # 新创建的txt文件的存放路径
+        full_path = desktop_path + 'Parameter' + '.json'  # 也可以创建一个.doc的word文档
+        with open(full_path, 'w') as f:
+            f.write(js)
+        pass
+
+
+
+        # Initialize input
+    def input_initialization(self, env_info):
+        state = env_info[0]  # laser info + self state
+        state_set = []
+        for i in range(self.Num_skipFrame * self.Num_stackFrame):
+            state_set.append(state)
+        state_stack = np.zeros((self.Num_stackFrame, self.Num_dataSize))
+        for stack_frame in range(self.Num_stackFrame):
+            state_stack[(self.Num_stackFrame - 1) - stack_frame,
+                        :] = state_set[-1 - (self.Num_skipFrame * stack_frame)]
+
+        observation = env_info[1]  # image info
+        observation_set = []
+        for i in range(self.Num_skipFrame * self.Num_stackFrame):
+            observation_set.append(observation)
+        # 使用观察组根据跳帧和堆叠帧的数量堆叠帧
+        observation_stack = np.zeros(
+            (self.img_size, self.img_size, self.Num_stackFrame))
+        # print("shape of observation stack={}".format(observation_stack.shape))
+        for stack_frame in range(self.Num_stackFrame):
+            observation_stack[:, :, stack_frame] = observation_set[-1 -
+                                                                   (self.Num_skipFrame * stack_frame)]
+        observation_stack = np.uint8(observation_stack)
+
+        return observation_stack, observation_set, state_stack, state_set
+
+
+    # Resize input information
+    def resize_input(self, env_info, observation_set, state_set):
+        observation = env_info[1]
+        observation_set.append(observation)
+        # 使用观察组根据跳帧和堆叠帧的数量堆叠帧
+        observation_stack = np.zeros(
+            (self.img_size, self.img_size, self.Num_stackFrame))
+        for stack_frame in range(self.Num_stackFrame):
+            observation_stack[:, :, stack_frame] = observation_set[-1 -
+                                                                   (self.Num_skipFrame * stack_frame)]
+        del observation_set[0]
+        observation_stack = np.uint8(observation_stack)
+
+        state = env_info[0]
+        state_set.append(state)
+        state_stack = np.zeros((self.Num_stackFrame, self.Num_dataSize))
+        for stack_frame in range(self.Num_stackFrame):
+            state_stack[(self.Num_stackFrame - 1) - stack_frame,
+                        :] = state_set[-1 - (self.Num_skipFrame * stack_frame)]
+
+        del self.state_set[0]
+        return observation_stack, observation_set, state_stack, state_set
+
+
+    def get_progress(self, step):
+        if step <= self.Num_start_training:
+            # Obsersvation
+            progress = 'Observing'
+            # Epsilon =self.Epsilon
+
+        elif step <= self.Num_start_training + self.Num_training:
+            # Training
+            progress = 'Training'
+            if step<= self.Num_start_training + self.Num_training/4:
+                self.Epsilon=0.9
+            elif step<= self.Num_start_training + self.Num_training/2:
+                self.Epsilon=0.5
+            elif step<= self.Num_start_training + self.Num_training/4*3:
+                self.Epsilon=0.25
+            else:
+                self.Epsilon=0.1
+
+        elif step < self.Num_start_training + self.Num_training + self.Num_test:
+            # Testing
+            progress = 'Testing'
+            # Epsilon = 0
+
+        else:
+            # Finished
+            progress = 'Finished'
+            # Epsilon = 0
+
+        return progress
+
+    def select_action(self, progress, state_stack,image):
+        if progress == "Observing":
+            # 观察的情况下，随机选择一个action
+            # action = np.zeros([self.Num_action])
+            # action[random.randint(0, self.Num_action - 1)] = 1.0
+            action= random.randint(0, self.Num_action - 1) 
+        elif progress == "Training":
+            # if random.random() < self.Epsilon:
+            #     action= random.randint(0, self.Num_action - 1) 
+            # #     # action = np.zeros([self.Num_action])
+            # #     # action[random.randint(0, self.Num_action - 1)] = 1
+            # #     a0 = random.uniform(0, 1) 
+            # #     a1 = random.uniform(-1, 1) 
+            # #     action=[a0,a1]
+            # else:
+            action=self.PPO.choose_action([state_stack],[image])
+        else:
+            # 动作是具有最大Q值的动作
+            # action_actor = self.output_actor.eval(
+            #     feed_dict={self.x_image: [observation_stack], self.x_sensor: [state_stack]})
+            # p = action_actor.ravel()
+            # action[np.argmax(p)] = 1
+            action=self.PPO.choose_action([state_stack],[image])
+        return action
+
+
+    def main(self):
+        reward_list = []
+        # 随机种子
+        random.seed(1000)
+        np.random.seed(1000)
+        tf.set_random_seed(1234)
+        # 随机初始化起点和终点的位置
+        while(True):
+            randposition = 2 * self.d * \
+                np.random.random_sample((2, 2)) - self.d
+            if math.sqrt((randposition[0][0]-randposition[1][0])**2+(randposition[0][1]-randposition[1][1])**2) > 20.0:
+                break
+        env.reset_env(start=[randposition[0][0], randposition[0][1]], goal=[randposition[1][0], randposition[1][1]])
+        env_info = env.get_env()
+        # env.info为4维，第1维为相机消息，第2维为agent robot的self state，第3维为terminal，第4维为reward
+        observation_stack, self.observation_set, state_stack, self.state_set= self.input_initialization(env_info)
+        state_stack_x=env_info[0][-4:]
+        step_for_newenv = 0
+        buffer_state,buffer_image, buffer_a, buffer_r = [], [], [],[]
+        v=0
+
+        # Training & Testing
+        while True:
+            # Get Progress, train mode 获取当前的进度和train_mode的值
+            self.progress = self.get_progress(self.step)
+            # Select Actions 根据进度选取动作
+            action = self.select_action(self.progress, state_stack_x,observation_stack)
+            env.step(action_dict[action])
+            env_info = env.get_env()
+            a = np.zeros([self.Num_action])
+            a[action]=1
+            next_observation_stack, self.next_observation_set, next_state_stack, self.next_state_set= self.resize_input(env_info, self.observation_set, self.state_set)  # 调整输入信息
+            next_state_stack_x=env_info[0][-4:]
+
+            terminal = env_info[-2]  # 获取terminal
+            reward = env_info[-1]  # 获取reward
+
+            if step_for_newenv == self.MAXSTEPS:
+                terminal = True
+
+            if self.progress == 'Training':
+                # buffer_observation.append(observation_stack)
+                buffer_state.append(state_stack_x)
+                buffer_image.append(observation_stack)
+                buffer_a.append(a)
+                # buffer_r.append((reward+8)/8)    # normalize reward, find to be useful
+                buffer_r.append(reward)    # normalize reward, find to be useful
+                # print(buffer_a)
+                if (step_for_newenv+1)%self.Num_batch==0 or terminal == True:
+                    v,probs=self.PPO.train(buffer_state,buffer_image,buffer_r, buffer_a,self.step-self.Num_start_training,[next_state_stack_x],[next_observation_stack])
+                    buffer_state,buffer_image,buffer_a, buffer_r =  [], [],[],[]
+                    with open('Empty/saved_networks/'+self.PPO.date_time+'/value.txt','a') as f:             
+                        f.write('reward')
+                        f.write('\r')
+                        np.savetxt(f, [reward], delimiter=',', fmt = '%s')
+                        f.write('action')
+                        f.write('\r')
+                        np.savetxt(f, action_dict[action], delimiter=',', fmt = '%s')
+                        f.write('v')
+                        f.write('\r')
+                        np.savetxt(f, [v], delimiter=',', fmt = '%s')
+                        f.write('probs')
+                        f.write('\r')
+                        np.savetxt(f, probs, delimiter=',', fmt = '%s')
+                        f.write('Num_train')
+                        f.write('\r')
+                        np.savetxt(f, [self.step-self.Num_start_training], delimiter=',', fmt = '%s')
+                        f.write('\r')
+
+
+            # If progress is finished -> close!
+            if self.progress == 'Finished' or self.episode == self.MAXEPISODES:
+                print('Finished!!')
+                break
+
+            # Update information
+            self.step += 1
+            self.score += reward
+            observation_stack = next_observation_stack
+            state_stack_x = next_state_stack_x
+            step_for_newenv = step_for_newenv + 1
+
+
+            # If terminal is True
+            if terminal == True:
+                self.PPO.save_model()
+                step_for_newenv = 0
+                # Print informations
+                print('step:'+str(self.step)+'/'+'episode:'+str(self.episode)+'/'+'progress:' +self.progress+'/'+'/'+'score:' + str(self.score))
+                if str(self.score)=="nan":
+                    sys.exit(0)
+
+
+                if self.progress == 'Training':
+                    reward_list.append(self.score)
+                    reward_array = np.array(reward_list)
+                    # ------------------------------
+                    np.savetxt('Empty/saved_networks/'+self.PPO.date_time+'/Reward.txt', reward_array, delimiter=',')
+                    # ------------------------------
+                if self.progress != 'Observing':
+                    self.episode += 1
+
+                self.score = 0
+
+                # Initialize game state
+                # 随机初始化起点和终点的位置
+                while(True):
+                    randposition = 2 * self.d * \
+                        np.random.random_sample((2, 2)) - self.d
+                    if math.sqrt((randposition[0][0]-randposition[1][0])**2+(randposition[0][1]-randposition[1][1])**2) > 20.0:
+                        break
+                env.reset_env(start=[randposition[0][0], randposition[0][1]], goal=[
+                              randposition[1][0], randposition[1][1]])
+                env_info = env.get_env()
+                state_stack_x=env_info[0][-4:]
+        pass
+
+
+if __name__ == '__main__':
+    agent = play()
+    agent.main()
